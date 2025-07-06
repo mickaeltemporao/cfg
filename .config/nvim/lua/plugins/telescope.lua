21d3e7c3eb87402c70e1348131ab9441a63bb544
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2025-07-06 19:15:23 +0200

    updated to os

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,27 +1,35 @@
 return {
-    "nvim-telescope/telescope.nvim", branch = "0.1.x",
-    cmd = {"Telescope"},
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      "nvim-telescope/telescope-bibtex.nvim",
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          require('telescope').load_extension('fzf')
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
-    opts = {
-      defaults = {
-        mappings = {
-          i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
-          },
-        },
-      },
-    }
+  'nvim-telescope/telescope.nvim', branch = '0.1.x',
+  cmd = {"Telescope"},
+  dependencies = {
+  'nvim-lua/plenary.nvim',
+  }
 }
 
+-- return {
+--     "nvim-telescope/telescope.nvim", branch = "0.1.x",
+--     cmd = {"Telescope"},
+--     dependencies = {
+--       'nvim-lua/plenary.nvim',
+--       "nvim-telescope/telescope-bibtex.nvim",
+--       {
+--         'nvim-telescope/telescope-fzf-native.nvim',
+--         build = 'make',
+--         cond = function()
+--           require('telescope').load_extension('fzf')
+--           return vim.fn.executable 'make' == 1
+--         end,
+--       },
+--     },
+--     opts = {
+--       defaults = {
+--         mappings = {
+--           i = {
+--             ['<C-u>'] = false,
+--             ['<C-d>'] = false,
+--           },
+--         },
+--       },
+--     }
+-- }
+--
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 dotfiles using git bare folder : https://www.atlassian.com/git/tutorials/dotfiles
+For Fish see : https://coffeeaddict.dev/how-to-manage-dotfiles-with-git-bare-repo/ 
