80fd7dee6113118d88d67e089c80821770d9dd10
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2025-09-01 22:16:33 +0200

    adding zelig test

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,8 +1,8 @@
 return {
-  "Mofiqul/dracula.nvim",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd[[colorscheme dracula]]
+    vim.cmd[[colorscheme tokyonight]]
   end
 }
